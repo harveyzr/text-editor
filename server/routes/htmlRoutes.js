bfa60d3e2e5e0502c7097294a8a0f6fd3b52d9d0
harveyzr <harveyzr@gmail.com>
2024-07-23 20:43:47 -0400

    comments

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,11 +1,14 @@
 // Import the path module to handle file paths
 const path = require('path');
 
-// Export a function that sets up a route for the application
+// Export a function that sets up routes for the application
 module.exports = (app) => {
   // Define a GET route for the root URL
   app.get('/', (req, res) => {
-    // Send the index.html file located in the client/dist directory
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    // Construct the path to the index.html file in the client/dist directory
+    const indexPath = path.join(__dirname, '../client/dist/index.html');
+    
+    // Send the index.html file as a response
+    res.sendFile(indexPath);
   });
 };
